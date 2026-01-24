47eeb93fc62393dbc3ae0fc1e49e497f67f0a2c6
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-24 20:09:34 +0100

    y

--- scripts/start-vm.sh
+++ scripts/start-vm.sh
@@ -33,7 +33,7 @@ echo "##### COPY XFCE CONFIG #####"
 
 cp -rfv ./profiles/vm/xfce4 ~/.config/
 cp -rfv ./profiles/vm/Thunar ~/.config/
-cp -rfv ./profiles/minis/gtk-3.0 ~/.config/
+cp -rfv ./profiles/vm/gtk-3.0 ~/.config/
 
 echo "##### LOAD minis PANEL CONFIG #####"
 
