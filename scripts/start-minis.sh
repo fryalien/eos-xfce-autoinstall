d927ca1715222e0d9f87f9d875049ea0b436e0a6
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-24 19:30:18 +0100

    y

--- scripts/start-minis.sh
+++ scripts/start-minis.sh
@@ -6,24 +6,6 @@ date
 
 ######################################################################################
 
-echo "##### SET SUDO 3 hrs #####"
-
-./scripts/set-sudo-timeout.sh
-
-######################################################################################
-
-echo "##### SET PRESENTATION MODE ON #####"
-
-xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true
-
-######################################################################################
-
-echo "##### STOP PERIODIC PING #####"
-
-./scripts/stop-ping.sh
-
-######################################################################################
-
 echo "##### INSTALL EOS ALL #####"
 
 ./scripts/eos-xfce-install.sh
