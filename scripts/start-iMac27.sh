8f578889d083ac6cf6835ce6a6fa38eacf666aa4
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-25 16:41:58 +0100

    y

--- scripts/start-iMac27.sh
+++ scripts/start-iMac27.sh
@@ -18,6 +18,10 @@ sh ./profiles/iMac27/flatpak-apps.sh
 
 flatpak override --user --filesystem=~/BIGDATA
 
+echo "##### COPY FLATPAK CONFIG #####"
+
+sh ./profiles/iMac27/flatpak-config.sh
+
 ######################################################################################
 
 echo "##### COPY XFCE CONFIG #####"
